9d7c90df38f939edae1669fc7bde369ff856b31c
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-05-10 13:56:01 -0500

    progress

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ const n4 = 25;
 
 
 //Check if all numbers are divisible by 5
+const allDivisibleBy5 = n1 % 5 === 0 && n2 % 5 === 0 && n3 % 5 === 0 && n4 % 5 === 0;
+console.log( 'All number are divisbkle by 5: ${all Divisbleby5}');
+
+// Check if the first number is larger than the last
+const firstLargerThanLast = n1 > n4;
+console.log('First number is larger than the last: ${firstLargerThanLast}');
+
